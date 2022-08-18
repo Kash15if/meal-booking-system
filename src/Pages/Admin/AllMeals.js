ebcbe8ef8bec04c193ae8ab7e4af7b33c91ffed1
Kash15if <kash15if@yahoo.com>
2022-08-19 00:36:39 +0530

    Adding functionlity to All Meals in the admin page

--- src/Pages/Admin/AllMeals.js
+++ src/Pages/Admin/AllMeals.js
@@ -1,29 +1,55 @@
 import FilterableTable from "../../CustomComponents/Table/FilterableTable";
-
+import axios from "axios";
+import { useState, useEffect } from "react";
 const AllMeals = () => {
+  const [tabData, setTabData] = useState();
+
+  const [tabHeader, setTabHeader] = useState();
+
+  useEffect(() => {
+    fetchAllMeals();
+  }, []);
+
+  const fetchAllMeals = async () => {
+    const endPoint = process.env.REACT_APP_BASE_URL_ADMIN + "allmeals";
+
+    try {
+      const res = await axios({
+        method: "GET",
+        url: endPoint,
+        headers: {
+          "Content-Type": "application/json;charset=UTF-8",
+          "Access-Control-Allow-Origin": "*",
+          "x-access-token": "Bearer " + localStorage.getItem("token"),
+        },
+      });
+
+      let allData = res.data;
+      console.log(allData);
+      setTabData(
+        allData.map((item) => ({
+          ...item,
+          Date: new Date(item.Date).toISOString().split("T")[0],
+        }))
+      );
+
+      let tempHeader = Object.keys(allData[0]);
+      setTabHeader(tempHeader);
+    } catch (err) {
+      console.log(err);
+      // logOut();
+    }
+  };
+
   return (
     <div>
-      <FilterableTable
-        tabData={[
-          {
-            id: 1,
-            name: "Kashif",
-            ph: "90909001",
-          },
-          {
-            id: 2,
-            name: "Faraz",
-            ph: "90909002",
-          },
-          {
-            id: 3,
-            name: "Rayan",
-            ph: "90909001",
-          },
-        ]}
-        header={["id", "name", "ph"]}
-        filterableColumn={["name", "ph"]}
-      />
+      {tabData && tabHeader && (
+        <FilterableTable
+          tabData={tabData}
+          header={tabHeader}
+          filterableColumn={["UserId", "Date"]}
+        />
+      )}
     </div>
   );
 };
