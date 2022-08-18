6487a3e930efdceea433ffdd9fa362b14b172146
Kash15if <kash15if@yahoo.com>
2022-08-18 22:30:27 +0530

    added input text or number for extra meal

--- src/Components/User/MealBookingTabComp.js
+++ src/Components/User/MealBookingTabComp.js
@@ -15,6 +15,15 @@ const MealTabRows = ({ index, row, handleToggleChange }) => {
     // setMealOn(!mealOn);
   };
 
+  const handleExtraMeal = (e) => {
+    const { name, value } = e.target;
+
+    const tempObj = { ...row, [name]: value };
+    // console.log(tempObj);
+    handleToggleChange(tempObj, index);
+    // setMealOn(!mealOn);
+  };
+
   const handleExtra = (e) => {
     setExtra(!extra);
   };
@@ -38,16 +47,15 @@ const MealTabRows = ({ index, row, handleToggleChange }) => {
         </div>
       </td>
       <td>
-        <div className="switchBtn">
-          <label className="switch">
-            <input
-              name="Extra_Meal"
-              type="checkbox"
-              checked={row.Extra_Meal === 1 ? true : false}
-              onChange={(e) => handleMealOnChange(e)}
-            />
-            <span className="slider round"></span>
-          </label>
+        <div className="extraMeal">
+          <input
+            name="Extra_Meal"
+            type="number"
+            min={0}
+            max={5}
+            value={row.Extra_Meal}
+            onChange={(e) => handleExtraMeal(e)}
+          />
         </div>
       </td>
     </tr>
